f7e1f3c44fc52c6038818e8037fe9a59f02f8b1e
Yordanos-zegeye <yordanoszegeye24@gmail.com>
2024-04-20 14:04:30 +0300

    created the booking page

---
+++ lib/presentation/screens/booking_page.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:hotel_booking/presentation/widgets/appbar.dart';
+import 'package:hotel_booking/presentation/test_input/datas.dart';
+import 'package:hotel_booking/presentation/widgets/drawer.dart';
+
+
+
+
