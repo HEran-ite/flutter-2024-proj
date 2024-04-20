60e32d4673da3694c27b89821007f92057ea2b87
HEran-ite <137596848+HEran-ite@users.noreply.github.com>
2024-04-20 19:22:38 +0300

    Update booking_page.dart

--- lib/presentation/screens/booking_page.dart
+++ lib/presentation/screens/booking_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:carousel_slider/carousel_slider.dart';
 import 'package:hotel_booking/presentation/widgets/appbar.dart';
 import 'package:hotel_booking/presentation/test_input/datas.dart';
 import 'package:hotel_booking/presentation/widgets/drawer.dart';
@@ -28,78 +27,14 @@ class BookingPage extends StatelessWidget {
                   ),
                 ),
                 const SizedBox(height: 20),
-                CarouselSlider.builder(
-                  itemCount: category['images'].length,
-                  options: CarouselOptions(
-                    height: 465,
-                    enlargeCenterPage: true,
-                    autoPlay: true,
-                    aspectRatio: 16 / 9,
-                    autoPlayCurve: Curves.fastOutSlowIn,
-                    enableInfiniteScroll: true,
-                    autoPlayAnimationDuration: Duration(milliseconds: 800),
-                    viewportFraction: 0.8,
+                SizedBox(
+                  height: 465,
+                  child: PageView.builder(
+                    itemCount: category['images'].length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return _buildCard(context, category, index);
+                    },
                   ),
-                  itemBuilder:
-                      (BuildContext context, int index, int realIndex) {
-                    return Card(
-                      color: Color.fromARGB(255, 244, 229, 212),
-                      elevation: 5.0, // Elevation for shadow effect
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            Image.asset(
-                              category['images'][index],
-                              height: 200,
-                              width: double.infinity,
-                              fit: BoxFit.cover,
-                            ),
-                            Divider(
-                              color: const Color.fromARGB(255, 208, 188, 188),
-                            ),
-                            Text(
-                              ' ${category['descriptions'][index]}',
-                              textAlign: TextAlign.center,
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 18,
-                                  color: Color.fromARGB(255, 95, 65, 65)),
-                            ),
-                            Text(
-                              'Price: \$${category['prices'][index]}',
-                              style: TextStyle(
-                                color: Color.fromARGB(255, 95, 65, 65),
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            ElevatedButton(
-                              onPressed: () {
-                                _showDatePickerDialog(context);
-                              },
-                              child: Text(
-                                'B O O K',
-                                style: TextStyle(
-                                    color:
-                                        const Color.fromARGB(255, 99, 76, 76)),
-                              ),
-                              style: ButtonStyle(
-                                backgroundColor:
-                                    MaterialStateProperty.all<Color>(
-                                        Color.fromARGB(255, 235, 231, 229)),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    );
-                  },
                 ),
               ],
             );
@@ -108,10 +43,61 @@ class BookingPage extends StatelessWidget {
       ),
     );
   }
-    // Function to show date picker dialog
+
+  Widget _buildCard(BuildContext context, Map<String, dynamic> category, int index) {
+    return Card(
+      margin: EdgeInsets.symmetric(horizontal: 40),
+      color: Color.fromARGB(255, 244, 229, 212),
+      elevation: 5.0, // Elevation for shadow effect
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Image.asset(
+              category['images'][index],
+              height: 200,
+              width: double.infinity,
+              fit: BoxFit.cover,
+            ),
+            const Divider(color: Color.fromARGB(255, 208, 188, 188)),
+            Text(
+              ' ${category['descriptions'][index]}',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18, color: Color.fromARGB(255, 95, 65, 65)),
+            ),
+            Text(
+              'Price: \$${category['prices'][index]}',
+              style: TextStyle(
+                color: Color.fromARGB(255, 95, 65, 65),
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                _showDatePickerDialog(context);
+              },
+              child: Text(
+                'B O O K',
+                style: TextStyle(color: Color.fromARGB(255, 99, 76, 76)),
+              ),
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(Color.fromARGB(255, 235, 231, 229)),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  // Function to show date picker dialog
   void _showDatePickerDialog(BuildContext context) async {
     DateTime? selectedDate = await showDatePicker(
-      
       context: context,
       initialDate: DateTime.now(),
       firstDate: DateTime.now(),
@@ -123,5 +109,3 @@ class BookingPage extends StatelessWidget {
     }
   }
 }
-
-
