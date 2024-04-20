582375a40b6d6a5648020650425467648eb1f493
soliyana <soliyanakewani228@gmail.com>
2024-04-20 12:45:29 +0300

    signup and login

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:hotel_booking/presentation/screens/admin_page.dart';
-import 'package:hotel_booking/presentation/screens/home_page.dart';
-
+import 'package:hotel_booking/presentation/screens/login_page.dart';
+import 'package:hotel_booking/presentation/screens/signup_page.dart';
 
 
 void main() {
@@ -16,12 +15,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      routes: {
-        '/home_page': (context) => HomePage(),
-        '/admin_page': (context) => AdminPage(),
-
+       routes: {
+        '/signup_page': (context) => SignUpPage(),
+        '/login_page': (context) => LoginPage(),
+      
       },
-      initialRoute: '/admin_page',
+      
+      initialRoute: '/signup_page',
     );
   }
 }
